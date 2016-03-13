b1a391eb0a039d7e35821be6ae6924d568127fdd
Tiina Partanen <tyynetyyne@gmail.com>
2016-03-13 14:07:45 +0200

    Updates

--- tehtavat/tiedostot/jatko/rekursio/pienenevat_pallot_esimerkkiratkaisuja.rkt
+++ tehtavat/tiedostot/jatko/rekursio/pienenevat_pallot_esimerkkiratkaisuja.rkt
@@ -3,12 +3,42 @@
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname pienenevat_pallot_esimerkkiratkaisuja) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
 (require 2htdp/image)
 
+;; RATKAISU1 (alkeistapaukseen perustuva)
 ;; pienenevät-pallot : Luku Väri -> Kuva
 (define (pienenevät-pallot säde väri)
    (if (<= säde 1)
        (circle säde "solid" väri)
        (beside
           (circle säde "solid" väri)
-          (pienenevät-pallot (* säde (/ 4 5)) väri))))
+          (pienenevät-pallot (* säde 0.8) väri))))
+
+(check-expect (pienenevät-pallot 1 "red")
+              (circle 1 "solid" "red"))
+
+(check-expect (pienenevät-pallot 2 "red")
+              (beside (circle 2 "solid" "red")
+                      (circle (* 2 0.8) "solid" "red")
+                      (circle (* 2 0.8 0.8) "solid" "red")
+                      (circle (* 2 0.8 0.8 0.8) "solid" "red")
+                      (circle (* 2 0.8 0.8 0.8 0.8) "solid" "red")))
 
 (pienenevät-pallot 30 "red")
+
+;; RATKAISU2 (akkumulaattoriin perustuva)
+;; pienenevät-pallot2 : Luku Väri Kuva -> Kuva
+(define (pienenevät-pallot2 säde väri valmis)
+   (if (<= säde 1)
+       (beside valmis (circle säde "solid" väri))
+       (pienenevät-pallot2 (* säde 0.8) väri (beside valmis (circle säde "solid" väri)))))
+                                              
+(check-expect (pienenevät-pallot2 1 "red" empty-image)
+              (circle 1 "solid" "red"))
+
+(check-expect (pienenevät-pallot2 2 "red" empty-image)
+              (beside (circle 2 "solid" "red")
+                      (circle (* 2 0.8) "solid" "red")
+                      (circle (* 2 0.8 0.8) "solid" "red")
+                      (circle (* 2 0.8 0.8 0.8) "solid" "red")
+                      (circle (* 2 0.8 0.8 0.8 0.8) "solid" "red")))
+
+(pienenevät-pallot2 30 "red" empty-image)
--- tehtavat/tiedostot/jatko/rekursio/tikkataulu_esimerkkiratkaisuja.rkt
+++ tehtavat/tiedostot/jatko/rekursio/tikkataulu_esimerkkiratkaisuja.rkt
@@ -3,55 +3,75 @@
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname tikkataulu_esimerkkiratkaisuja) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
 (require 2htdp/image)
 
-;; RATKAISU1 (vaihdetaan joka kierroksella värien järjestys)
-;; tikkataulu : Luku Luku -> Kuva
-(define (tikkataulu i väri1 väri2 kuva)
+;; RATKAISU1 (alkeistapauksiin perustuva + vaihdetaan värien järjestys joka kierroksella)
+;; tikkataulu : Luku Väri Väri -> Kuva
+(define (tikkataulu i väri1 väri2)
+  (if (<= i 0)
+      empty-image
+      (overlay (tikkataulu (sub1 i) väri2 väri1)
+               (circle (* i 10) "solid" väri1))))
+      
+(check-expect (tikkataulu 0 "blue" "red")
+              empty-image)
+
+(check-expect (tikkataulu 1 "blue" "red")
+              (circle 10 "solid" "blue"))
+
+(check-expect (tikkataulu 2 "blue" "red")
+              (overlay (circle 10 "solid" "red")
+                       (circle 20 "solid" "blue")))
+
+(tikkataulu 10 "red" "white")
+
+;; RATKAISU2 (akkumulaattoriin perustuva + vaihdetaan värien järjestys joka kierroksella)
+;; tikkataulu : Luku Luku Kuva -> Kuva
+(define (tikkataulu2 i väri1 väri2 kuva)
   (if (<= i 0)
       kuva
-      (tikkataulu (sub1 i)
+      (tikkataulu2 (sub1 i)
                   väri2
                   väri1
                   (overlay (circle (* i 10) "solid" väri1) kuva))))
 
-(check-expect (tikkataulu 1 "blue" "red" empty-image)
-              (circle 10 "solid" "blue"))
-
-(check-expect (tikkataulu 2 "blue" "red" empty-image)
-              (overlay (circle 10 "solid" "red")
-                       (circle 20 "solid" "blue")))
-
-(check-expect (tikkataulu 3 "blue" "red" empty-image)
-              (overlay (circle 10 "solid" "blue")
-                       (circle 20 "solid" "red")
-                       (circle 30 "solid" "blue")))
-               
-(tikkataulu 10 "red" "white" empty-image)
-
-;; RATKAISU2 (parittomalla kieroksella erilainen toiminta kuin parillisella)
-;; tikkataulu : Luku Luku -> Kuva
-(define (tikkataulu2 i väri1 väri2 kuva)
-  (if (<= i 0)
-      kuva
-      (if (even? i)
-          (tikkataulu2 (sub1 i)
-                      väri1
-                      väri2
-                      (overlay (circle (* i 10) "solid" väri1) kuva))
-          (tikkataulu2 (sub1 i)
-                      väri1
-                      väri2
-                      (overlay (circle (* i 10) "solid" väri2) kuva)))))
-
 (check-expect (tikkataulu2 1 "blue" "red" empty-image)
-              (circle 10 "solid" "red"))
+              (circle 10 "solid" "blue"))
 
 (check-expect (tikkataulu2 2 "blue" "red" empty-image)
               (overlay (circle 10 "solid" "red")
                        (circle 20 "solid" "blue")))
 
 (check-expect (tikkataulu2 3 "blue" "red" empty-image)
+              (overlay (circle 10 "solid" "blue")
+                       (circle 20 "solid" "red")
+                       (circle 30 "solid" "blue")))
+               
+(tikkataulu2 10 "red" "white" empty-image)
+
+;; RATKAISU3 (akkumulaattori + parittomalla kieroksella erilainen toiminta kuin parillisella)
+;; tikkataulu : Luku Väri Väri Kuva -> Kuva
+(define (tikkataulu3 i väri1 väri2 kuva)
+  (if (<= i 0)
+      kuva
+      (if (even? i)
+          (tikkataulu3 (sub1 i)
+                      väri1
+                      väri2
+                      (overlay (circle (* i 10) "solid" väri1) kuva))
+          (tikkataulu3 (sub1 i)
+                      väri1
+                      väri2
+                      (overlay (circle (* i 10) "solid" väri2) kuva)))))
+
+(check-expect (tikkataulu3 1 "blue" "red" empty-image)
+              (circle 10 "solid" "red"))
+
+(check-expect (tikkataulu3 2 "blue" "red" empty-image)
+              (overlay (circle 10 "solid" "red")
+                       (circle 20 "solid" "blue")))
+
+(check-expect (tikkataulu3 3 "blue" "red" empty-image)
               (overlay (circle 10 "solid" "red")
                        (circle 20 "solid" "blue")
                        (circle 30 "solid" "red")))
 
-(tikkataulu2 10 "red" "white" empty-image)
+(tikkataulu3 10 "red" "white" empty-image)
