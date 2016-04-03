cbe84b930ca6fa97514eff8c30886dc3a95b3f91
Tiina Partanen <tyynetyyne@gmail.com>
2016-04-03 21:32:10 +0300

    Added lists

---
+++ tehtavat/tiedostot/jatko/listat/keskiarvo_esimerkkiratkaisuja.rkt
@@ -0,0 +1,28 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname keskiarvo_esimerkkiratkaisuja) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+;; SUMMA
+;; summaa-lista : Lista<Luku> -> Luku
+(define (summaa-lista luvut)
+  (if (empty? luvut)
+      0
+      (+ (first luvut)
+         (summaa-lista (rest luvut)))))
+
+(check-expect (summaa-lista (list 1 2 3))
+              6)
+
+(check-expect (summaa-lista '())
+              0)
+
+;; keskiarvo : Lista<Luku> -> Luku
+(define (keskiarvo luvut)
+  (if (not (empty? luvut))
+      (/ (summaa-lista luvut) (length luvut))
+      0))
+
+(check-expect (keskiarvo (list 1 2 3 4 5))
+              3)
+
+(check-expect (keskiarvo '())
+              0)
