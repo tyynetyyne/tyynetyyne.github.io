9f288ffb5daa31ad44cea972e3f64b0697fea518
Tiina Partanen <tyynetyyne@gmail.com>
2016-10-20 11:25:38 +0300

    Lisätty symmetriatehtävät

---
+++ tehtavat/tiedostot/maol/prosentteja_kuvilla.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname prosentteja_kuvilla) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+
+(beside (above (rectangle 50 50 "solid" "red")
+               (rectangle 50 50 "solid" "red"))
+        (above (rectangle 50 50 "solid" "blue")
+               (rectangle 50 50 "solid" "yellow")))
