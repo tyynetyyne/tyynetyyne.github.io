f4a010cc1dc47a65687abe4fd985e1f8933653f1
Tiina Partanen <tyynetyyne@gmail.com>
2017-01-23 13:23:26 +0200

    otukset

--- tehtavat/tiedostot/maol/display-read_harjoittelua.rkt
+++ tehtavat/tiedostot/maol/display-read_harjoittelua.rkt
@@ -1,8 +1,15 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname display-read_harjoittelua) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname display-read_harjoittelua) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
 (require teachpacks/display-read)
-(require 2htdp/image)
+
+(define (ohjelma)
+  (begin (display-info "Hei tässä alkaa lohko")
+         (display-info "Nämä viestit ovat sivuvaikutuksia")
+         (display-info "Tämä on viimeinen sivuvaikutus")
+         "tämä merkkijono on paluuarvo"))
+
+(ohjelma)
 
 
 
