8cc83edd3c4a138843f445f8df30ac838a9f9153
Tiina Partanen <tyynetyyne@gmail.com>
2017-07-06 18:10:54 +0300

    Pallon heitto, kuvaajat

--- tehtavat/tiedostot/maol/funktiot_ja_kuvaajat.rkt
+++ tehtavat/tiedostot/maol/funktiot_ja_kuvaajat.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname kuvaajat) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname funktiot_ja_kuvaajat) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
 (require teachpacks/plot)
 
 ;; pisteiden sijoittaminen koordinaatistoon
@@ -15,8 +15,6 @@
 
 ;; funktion kuvaajan piirtääminen koordinaatistoon
 ;; -----------------------------------------------
-(require teachpacks/plot)
-
 ;; funktio
 (define (s t)
   (* 3 t))
