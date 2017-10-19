0d9dcef00f5e1b4ddd048c64b132f55c7b36799f
Timo Poranen <timo.t.poranen@gmail.com>
2017-10-19 17:05:27 +0300

    -

--- tehtavat/tiedostot/maol/muuttujat-hedelmia.rkt
+++ tehtavat/tiedostot/maol/muuttujat-hedelmia.rkt
@@ -1,6 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname muuttujat-hedelmia) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;;Harjoitustiedosto muuttujien käytöstä
 (define omenoita 5)
 (define päärynöitä 4)
 (define luumuja 10)
