d8d5d401c82ae61e22c3037f273cbeeab4ea7568
Timo Poranen <timo.t.poranen@gmail.com>
2017-10-24 15:20:39 +0300

    Racket 1 -materiaalin tehtävien ratkaisuja.

---
+++ tehtavat/tiedostot/maol/moniosainen-laskulauseke-ratkaisut.rkt
@@ -0,0 +1,33 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname moniosainen-laskulauseke-ratkaisut) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;;Racket 1
+;;1. Ohjelmointiympäristö
+;;1.3 Moniosainen laskulauseke
+;;Tehtäväsarja 2: Moniosainen laskulauseke
+
+;;Tehtävä 1 a)
+(+ 10 (- 12 19)) ;Vastaus: 3
+
+;;Tehtävä 1 b)
+(- 45 (+ 22 30)) ;Vastaus: -7
+
+;;Tehtävä 1 c)
+(+ 12 (- 13 14)) ;Vastaus: 11
+
+;;Tehtävä 2 a)
+(+ 4 (* 3 6)) ;Vastaus: 22
+
+;;Tehtävä 2 b)
+(- (* 6 5) 3) ;Vastaus: 27
+ 
+;;Tehtävä 2 c)
+(- 20 (* 3 5)) ;Vastaus: 5
+
+;;Tehtävä 3
+;;Ratkaisu riippuu oppilaan keksimästä laskukausekkeesta.
+
+;;Tehtävä 4
+(* 10000 999999) ;Vastaus 9999990000
+
+;;Racketissa pystyy laskemaan hyvinkin isoilla luvuilla.
---
+++ tehtavat/tiedostot/maol/peruslaskutoimituksia-ratkaisut.rkt
@@ -0,0 +1,43 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname peruslaskutoimituksia-ratkaisut) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;;Racket 1
+;;1. Ohjelmointiympäristö
+;;1.2 Peruslaskutoimitukset
+;;Tehtäväsarja 1: Peruslaskutoimituksia
+
+;;Tehtävä 1 a)
+(+ 5 9) ;Vastaus: 14
+
+;;Tehtävä 1 b)
+(+ 54 37) ;Vastaus: 91
+
+;;Tehtävä 1 c)
+(+ 123 66) ;Vastaus: 189
+
+;;Tehtävä 1 d)
+(+ 110 230) ;Vastaus: 340
+
+;;Tehtävä 2 a)
+(- 10 6) ;Vastaus: 4
+
+;;Tehtävä 2 b)
+(- 20 66) ;Vastaus: -46
+
+;;Tehtävä 2 c)
+(- 44 23) ;Vastaus: 21
+
+;;Tehtävä 2 d)
+(- 550 300) ;Vastaus: 250
+
+;;Tehtävä 3 a)
+(* 3 9) ;Vastaus: 27
+
+;;Tehtävä 3 b)
+(* 12 10) ;Vastaus: 120
+
+;;Tehtävä 3 c)
+(/ 10 2) ;Vastaus: 5
+
+;;Tehtävä 3 d)
+(/ 120 6) ;Vastaus: 20
