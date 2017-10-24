adfa3f8b50cd924df776ddf3ca96d545097ffc68
Timo Poranen <timo.t.poranen@gmail.com>
2017-10-24 18:15:26 +0300

    Prosenttitehtävien ratkaisut.

---
+++ tehtavat/tiedostot/maol/prosenttilaskut-ratkaisut.rkt
@@ -0,0 +1,25 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname prosenttilaskut-ratkaisut) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;;Racket 1
+;;4. Murto- ja prosenttiluvut
+;;4.2 Prosenttiluku
+;;Tehtäväsarja 1: Prosenttilaskuja
+
+;;Tehtävä 1 a)
+(* 0.3 40) ;Vastaus 12
+
+;;Tehtävä 1 b)
+(* 0.03 100) ;Vastaus 3
+
+;;Tehtävä 1 c)
+(* 0.76 200) ;Vastaus 152
+
+;;Tehtävä 2 d)
+(* 0.15 200) ;Vastaus 30
+
+;;Tehtävä 2 e)
+(* 0.2 80) ;Vastaus 16
+
+;;Tehtävä 2 f)
+(* 0.02 300) ;Vastaus 6
