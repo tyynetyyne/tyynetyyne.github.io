0868783dfc56e80535dc4ab3c7e5b0b71120209a
Tiina Partanen <tyynetyyne@gmail.com>
2018-08-16 10:35:35 +0300

    New file

---
+++ tehtavat/tiedostot/maol/arduino/potentiometrilla_ohjattu_dc_moottori/potentiometrilla_ohjattu_dc_moottori.ino
@@ -0,0 +1,20 @@
+int M_OHJAUS = 9;
+int POTIKKA = A0;
+int arvo = 0;
+int skaalattu_arvo = 0;
+
+void setup() {
+  pinMode(M_OHJAUS, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  arvo = analogRead(POTIKKA);
+  skaalattu_arvo = map(arvo, 0, 1023, 0, 255);
+
+  Serial.print("Potikan lukema: ");
+  Serial.println(arvo);
+  
+  analogWrite(M_OHJAUS, skaalattu_arvo);
+  delay(10);
+}
